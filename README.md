63eddfb5d838125050587408991c0cf5d941fc35
tsandyk8s@gmail.com <tsandyk8s@gmail.com>
2022-05-06 23:49:01 -0500

    Testing failed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # cloud
 All Cloud Provider Specific Infrastructure as Code (IAC) goes in here
 Example: aws, azure, gcp, oci
+
+# Syntax
+<PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE>
+Example: 
+PROVIDER:aws
+TYPE:security_group
+NAME:instance (Name of the resource)
+ATTRIBUTE: Name or one of the attributes exported by the resource.
+
+
--- terraform/aws/server/main.tf
+++ terraform/aws/server/main.tf
@@ -3,10 +3,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Define security list
+resource "aws_security_group" "my_web_security_group" {
+  name        = "terraform-example"
+  description = "Security group for EC2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
+
 # Create EC2 instance from AMID
-resource "aws_instance" "example" {
-  ami           = "ami-0022f774911c1d690"
-  instance_type = "t2.micro"
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0022f774911c1d690"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_web_security_group.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "Hello, World this is Deepni's WebSite" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
+
 
   tags = {
     Name = "terraform_example"
